b8eb50a51fc35b972b0424b4f73900b4ebad824c
tb1015 <thoms1015@gmail.com>
2021-08-15 00:16:09 +0700

    Latihan programming dasar python: tipe data, gerbang logika, if, looping, fungsi, pembuatan modul

---
+++ Modules/f1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 00:03:52 2021
+
+@author: tb
+"""
+def f1(a,b):
+    return a+b
+
+if __name__ == '__main__':
+    print(f1(2,3)) 
+    
+    ##ini apa outputnya?
---
+++ Modules/my.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 15 00:05:14 2021
+Importing fungsi f1 dari module f1
+
+@author: tb
+"""
+
+from f1 import f1
+
+print(f1(2,2))
